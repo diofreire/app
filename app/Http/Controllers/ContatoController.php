536fb4061c4752444d5df0cf7258133c52b53dfc
Diogo Freire <diogofre@gmail.com>
2024-03-08 17:51:53 -0300

    Tradução das mensagens de erro e melhor exibição dessas

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -25,13 +25,22 @@ class ContatoController extends Controller
     public function salvar(Request $request) {
 
         //realizar a validação dos dados do formulário recebidos no request
-        $request->validate([
+        $request->validate(
+            [
             'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
-            'email' => 'email|required',
+            'email' => 'email',
             'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+            ],
+            [
+               'required' => 'O campo :attribute precisa ser preenchido',
+               'motivo_contatos_id' => 'O campo Motivo Contato precisa ser preenchido',
+               'nome.min' => 'O campo :attribute precisa ter no mínomo 3 caracteres',
+               'nome.max' => 'O campo :attribute precisa ter no máximo 40 caracteres',
+               'email.email' => 'O campo :attribute precisa ser um e-mail válido',
+            ]
+        );
 
         SiteContato::create($request->all());
         return redirect()->route('site.index');
