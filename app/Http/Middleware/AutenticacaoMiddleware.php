83e6128278e466e51bd57d32554bea7f6ff30f0f
Diogo Freire <diogofre@gmail.com>
2024-03-12 18:10:12 -0300

    Adicionado parametro no middleware de Autenticacao

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -13,12 +13,27 @@ class AutenticacaoMiddleware
      *
      * @param Request $request
      * @param Closure $next
+     * @param string $metodoAutenticacao
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
-    {
+    public function handle(
+        Request $request,
+        Closure $next,
+        string $metodoAutenticacao,
+        string $perfil
+    ) {
+
+        echo $metodoAutenticacao .' - '. $perfil;
+        echo "<br/>";
         // Verifica se o usuário possui acesso a rota
-        if(true) {
+        if($metodoAutenticacao == 'padrao') {
+            echo "Verifica o usuário e senha no banco de dados";
+        } elseif($metodoAutenticacao == 'ldap') {
+            echo "Verifica o usuário e senha no banco de AD";
+        }
+        echo "<br/>";
+
+        if(false) {
             return $next($request);
         } else {
             return Response('Acesso negado! Rota exige autenticação');
